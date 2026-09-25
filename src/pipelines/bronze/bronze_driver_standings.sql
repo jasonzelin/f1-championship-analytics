81fd73b7edc39660259284eaafdfb9a14968562b
jr9808@gmail.com <jr9808@gmail.com>
2026-09-25 08:24:31 +0000

    fix: fix schemas in select statement

--- src/pipelines/bronze/bronze_driver_standings.sql
+++ src/pipelines/bronze/bronze_driver_standings.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS f1_analytics.bronze.driver_standings
 AS
 SELECT
-    circuitId, circuitRef, name, location, country, lat, lng, alt, url
+    *
 FROM
     read_files(
         "$DATA_SOURCE_PATH",
