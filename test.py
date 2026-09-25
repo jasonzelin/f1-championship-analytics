fc41c7bc1f98e61283e7dd48279a2194aa7b261e
jr9808@gmail.com <jr9808@gmail.com>
2026-09-25 06:32:19 +0000

    fix: mask data source path into parameter

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-from src import data_ingestion
+import os
+import kaggle
 
-data_ingestion.download_data()
+import dotenv
+
+dotenv.load_dotenv()
+
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files('formula-1-world-championship-1950-2020', path='../../../Users/jr9808@gmail.com/data/raw/', unzip=True)
+
+spark.sql("""
+CREATE TABLE IF NOT EXISTS f1_analytics.bronze.circuits
+USING CSV
+LOCATION '/Workspace/Users/jr9808@gmail.com/data/raw/circuits.csv'
+OPTIONS (header = true, inferSchema = true)
+""")
+
+spark.sql("SELECT * FROM f1_analytics.bronze.circuits LIMIT 5").show()
